c6550c0a55d27332e8defa5a0d5f2c839776fa94
June Young Kang <jkang34@gmail.com>
2020-11-02 12:35:54 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -56,10 +56,9 @@ def runner
   card_total = initial_round
   hit?(card_total)
   display_card_total(card_total)
-  if card_total < 21
-    end_game(card_total)
-  else 
+  until card_total > 21 
     card_total = hit?(card_total)
     display_card_total(card_total)
   end
+  end_game(card_total)
 end
