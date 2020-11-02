ee215c56b60ae30ccf9dd7241a62c48f92a08a48
June Young Kang <jkang34@gmail.com>
2020-11-02 12:20:53 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -15,7 +15,7 @@ def prompt_user
 end
 
 def get_user_input
-  gets.chomp
+  input = gets.chomp
 end
 
 def end_game(card_total)
@@ -36,10 +36,10 @@ end
 
 def hit?(card_total)
   prompt_user
-  get_user_input
-  if get_user_input = 's' 
+  input = get_user_input
+  if input = 's' 
     return card_total
-  elsif get_user_input = 'h' 
+  elsif input = 'h' 
     card_total += deal_card
   else 
     invalid_command
